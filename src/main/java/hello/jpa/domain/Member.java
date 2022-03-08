3cf8162248c9424fefa5a1505b2ed2ea46108a82
jyh4358 <jyh4358@gmail.com>
2022-03-08 18:54:57 +0900

    JPA 연관관계 매핑

--- src/main/java/hello/jpa/domain/Member.java
+++ src/main/java/hello/jpa/domain/Member.java
@@ -4,10 +4,9 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
@@ -16,6 +15,9 @@ public class Member {
     @Id @GeneratedValue
     @Column(name = "MEMBER_ID")
     private Long id;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
 
     private String city;
     private String street;
